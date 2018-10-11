ffcbdca21bc7a45dea2e5d6473472412f9305958
Mary Ogbe <maryogbe09@yahoo.com>
2018-10-11 09:56:30 +0100

    I set up grunt

---
+++ gruntfile.js
@@ -0,0 +1,346 @@
+'use strict';
+var jsBaseDir = 'src/js/';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        connect: {
+            devServer: {
+                options: {
+                    livereload: true,
+                    port: 9000,
+                    base: ''
+                }
+            }
+        },
+
+
+        jasmine : {
+            src: 'src/js/custom/*.js',
+            options: {
+                vendor: [
+                    'http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js',
+                    'http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js',
+                    'http://cdnjs.cloudflare.com/ajax/libs/bootstrap-3-typeahead/4.0.2/bootstrap3-typeahead.min.js',
+                    'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.11.2/js/bootstrap-select.min.js'
+                ],
+                specs: 'src/js/custom/specs/**/*spec.js',
+                helpers: 'path/to/specs'
+            }
+        },
+
+        // css task
+        css: {
+            examples: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMapContents: true,
+                    sourceMap: true,
+                    precision: 4
+                },
+                files:{
+                    'css/stylecss.css': 'src/css/style.css'
+                }
+            },
+        },
+        sass: {
+            examples: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMapContents: true,
+                    sourceMap: true,
+                    precision: 4
+                },
+                files:{
+                    'css/stylesass.css': 'src/sass/style.sass'
+                }
+            },
+            hybris: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMapContents: true,
+                    sourceMap: true,
+                    precision: 4
+                },
+                files:{
+                    'css/stylesass.css': 'src/sass/style_hybris.sass'
+                }
+            },
+        },
+
+        // JS concatenation task for third-party scripts
+        uglify: { 
+            all_src : { 
+              options : { 
+                sourceMap : true, 
+                sourceMapName : 'sourceMap.map'
+              }, 
+              src : ['!src/js/third-party/jquery.min.js','src/js/third-party/bootstrap.min.js','src/js/third-party/moment.js',
+              'src/js/third-party/collapse.js', 'src/js/third-party/bootstrap-select.js', 'src/js/third-party/magicsuggest-min.js',
+                'src/js/third-party/datepicker.js', 'src/js/third-party/tether.js', 'src/js/third-party/jquery-ui-1.11.2.min.js',
+                'src/js/third-party/jquery-ui.multidatespicker.js', 'src/js/third-party/typeahead.js', 'src/js/third-party/jquery.creditCardValidator.js',
+                'src/js/third-party/jquery.validate.min.js', 'src/js/third-party/jquery.dataTables.min.js', 'src/js/third-party/jquery.mask.min.js',
+                'src/js/third-party/jquery.mCustomScrollbar.concat.min.js'
+              ], 
+              dest : 'src/js/third-party/composite.all.min.js'
+            } 
+        },
+
+
+        //  JS hint task
+        // jshint: {
+        //     options: {
+        //         jshintrc: '.jshintrc',
+        //         verbose: true,
+        //     },
+        //     all:[
+        //         'gruntfile.js',
+        //         jsBaseDir + '/custom/*.js',
+        //         '!' + jsBaseDir + '/*.js',
+        //         '!' + jsBaseDir + '/third-party/*.js'
+        //     ]
+        // },
+
+        // copy JS files task
+        sync: {
+            options: {
+                paths: ''
+            },
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['js/**/*.js'],
+                    dest: ''
+                }],
+                verbose: true
+            },
+            awesomeFonts: {
+                files: [{
+                    expand: true,
+                    cwd: 'node_modules/font-awesome',
+                    src: ['fonts/**/*.*'],
+                    dest: ''
+                }],
+                verbose: true
+            },
+            fonts: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['fonts/**/*.*'],
+                    dest: ''
+                }],
+                verbose: true
+            },
+            images: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: ['images/**'],
+                    dest: ''
+                }],
+                verbose: true
+            },
+
+            moveStaticFiles: {
+                files: [{
+                    expand: true,
+                    cwd: 'templates',
+                    src: ['**/*.*'],
+                    dest: ''
+                }],
+                verbose: true
+            }
+        },
+
+        
+
+        // *********   WATCH task **************************
+        watch: {
+            css: {
+                files: ['src/css/**/*.css'],
+                // tasks: ['css:examples'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['src/js/**/*.js'],
+                tasks: ['sync'],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+            test: {
+                files: [
+                    'gruntfile.js',
+                    jsBaseDir + '/custom/*.js',
+                    '!' + jsBaseDir + '/*.js',
+                    '!' + jsBaseDir + '/third-party/*.js'
+                ],
+                // tasks: ['jshint'],
+            },
+            css: {
+                files: ['css/**/*.css'],
+                tasks: 'postcss',
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+            html: {
+                files: [
+                    'src/**/*.html',
+                    'src/**/*.njk'
+                ],
+                tasks: [
+                    'clean:html',
+                    'nunjucks',
+                    'prettify',
+                    'sync'
+                ],
+                options: {
+                    spawn: false,
+                    livereload: true
+                }
+            },
+            gruntfile: {
+                files: 'gruntfile.js',
+                options: {
+                    spawn: false,
+                    livereload: true,
+                    reload: true
+                }
+            }
+        },
+
+        // Nunjucks task
+        // nunjucks: {
+        //     options: {
+        //         paths: 'src/html'
+        //     },
+        //     dev: {
+        //         files: [{
+        //             expand: true,
+        //             cwd: 'src/html',
+        //             src: [
+        //                 '**/*.html'
+        //             ],
+        //             dest: '',
+        //             ext: '.html'
+        //         }],
+        //     }
+        // },
+
+        // Clean task
+        clean: {
+            html: {
+                src: [
+                    'index.html',
+                    // 'examples/**/*.html'
+                ]
+            },
+            css: {
+                src: ['css/*.css']
+            },
+            all: {
+                src: [
+                    'index.html',
+                    // 'examples/**/*.html',
+                    'css/*.css'
+                ]
+            }
+        },
+
+        // Prettify task
+        prettify: {
+            options: {
+                'indent': 1,
+                'indent_char': ' ',
+                'wrap_line_length': 250,
+                'brace_style': 'collapse',
+                'preserve_newlines': true,
+                'condense': true,
+                'max_preserve_newlines': 2,
+                'unformatted': ['a', 'code', 'pre']
+            },
+            all: {
+                expand: true,
+                cwd: '',
+                src: ['templates/**/*.html'],
+                dest: '',
+                ext: '.html'
+            }
+        },
+
+
+        accessibility: {
+            options : {
+                accessibilityLevel: 'WCAG2A',
+                domElement: false,
+                force: true,
+                ignore : [
+                    'WCAG2A.Principle1.Guideline1_1.1_1_1.G73,G74',
+                    'WCAG2A.Principle2.Guideline2_4.2_4_4.H77,H78,H79,H80,H81'
+                ],
+                reportLevels: {
+                    notice: false,
+                    warning: false,
+                    error: true
+                }
+            },
+            // test : {
+            //     src: ['templates/manage-customer.html']
+            // }
+        }
+    });
+
+    // Load the plugins to run your tasks
+    require('load-grunt-tasks')(grunt, {
+        scope: 'devDependencies'
+    });
+
+    // require('time-grunt')(grunt);
+
+    // grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    // grunt.loadNpmTasks('grunt-accessibility');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    
+    // Default task(s).
+    grunt.registerTask('default', [
+        'clean:all',
+        // 'css:examples',
+        // 'jshint',
+        'prettify',
+        'connect:devServer',
+        'watch'
+    ]);
+
+    // Default task(s).
+    grunt.registerTask('hybris', [
+        'clean:all',
+        // 'jshint',
+        'prettify',
+        'connect:devServer',
+        'watch'
+    ]);
+
+    // grunt.registerTask('serve-up', ['sync:moveStaticFiles', 'connect:devServer']);
+    // grunt.registerTask('move-static', 'connect:moveStaticFiles');
+    grunt.registerTask('test-js', 'jasmine');
+    // grunt.registerTask('check-js', ['jshint']);
+    grunt.registerTask('compile-css', ['css']);
+    // grunt.registerTask('check-accessibility', ['accessibility']);
+    grunt.registerTask('concatJS', ['uglify']);
+    
+
+};
